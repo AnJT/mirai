d6911a11dd55cc27b37355cec694da65d7b89b34
番茄炒鸡蛋 <342472121@qq.com>
2021-02-20 21:45:45 +0800

    使用wait

--- code/electricity.py
+++ code/electricity.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 import requests
@@ -11,21 +15,27 @@ def getElectricity():
     chrome_options.headless=True
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
+    WAIT=WebDriverWait(driver,10)
 
     campus_value='9'
     building_value='20'
     floor_value='2005'
     room_value='200508'
+    
+    campus=WAIT.until(EC.presence_of_element_located((By.NAME,'drlouming')))
+    Select(campus).select_by_value(campus_value)
+    building=WAIT.until(EC.presence_of_element_located((By.NAME,'drceng')))
+    Select(building).select_by_value(building_value)
+    floor=WAIT.until(EC.presence_of_element_located((By.NAME,'dr_ceng')))
+    Select(floor).select_by_value(floor_value)
+    room=WAIT.until(EC.presence_of_element_located((By.NAME,'drfangjian')))
+    Select(room).select_by_value(room_value)
 
-    Select(driver.find_element_by_name('drlouming')).select_by_value(campus_value)
-    Select(driver.find_element_by_name('drceng')).select_by_value(building_value)
-    Select(driver.find_element_by_name('dr_ceng')).select_by_value(floor_value)
-    Select(driver.find_element_by_name('drfangjian')).select_by_value(room_value)
-
-    electricity_record=driver.find_element_by_id('usedR')
+    electricity_record=WAIT.until(EC.element_to_be_clickable((By.ID,'usedR')))
     electricity_record.click()
-    login=driver.find_element_by_id('ImageButton1')
+    login=WAIT.until(EC.element_to_be_clickable((By.ID,'ImageButton1')))
     login.click()
+    remaining_battery=WAIT.until(EC.presence_of_element_located((By.XPATH,'/html/body/form/div[3]/div[2]/div[1]/h6/span[1]')))
+    return remaining_battery.text
 
-    remaining_battery=driver.find_element_by_xpath('/html/body/form/div[3]/div[2]/div[1]/h6/span[1]').text
-    return remaining_battery
+print(getElectricity())
